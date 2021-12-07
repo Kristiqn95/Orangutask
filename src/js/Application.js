4f146de4914ff9e7c9427c0c0619166644b3cbfc
Kristiqn <kristiqn_estatiev@abv.bg>
2021-12-07 16:31:45 +0200

    feat: gave all monkeys a banana

--- src/js/Application.js
+++ src/js/Application.js
@@ -17,5 +17,12 @@ export default class Application extends EventEmitter {
     this.emojis = emojis;
   }
 
-  addBananas() {}
+  addBananas() {
+    this.emojis = this.emojis.map(emoji => emoji + this.banana);
+
+    const p = document.createElement("p");
+    p.innerText = this.emojis.reduce((prev, curr) => prev + curr);
+    const emojis = document.getElementById("emojis");
+    emojis.appendChild(p);
+  }
 }
